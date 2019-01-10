33911465862c89ab7d2071c3c4fc86f80128f3b7
yasunari.tanaka <yasu7ri@hotmail.com>
2019-01-11 08:19:58 +0900

    none

--- app/src/androidTest/java/com/example/appcenter/sampleapp_android/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/appcenter/sampleapp_android/ExampleInstrumentedTest.java
@@ -2,11 +2,28 @@ package com.example.appcenter.sampleapp_android;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
+import android.support.test.espresso.ViewInteraction;
 import android.support.test.runner.AndroidJUnit4;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
 
+import com.microsoft.appcenter.espresso.Factory;
+import com.microsoft.appcenter.espresso.ReportHelper;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.junit.After;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static android.support.test.espresso.Espresso.*;
+import static android.support.test.espresso.matcher.ViewMatchers.*;
+import static android.support.test.espresso.action.ViewActions.*;
+
+import static org.hamcrest.Matchers.allOf;
 import static org.junit.Assert.*;
 
 /**
@@ -16,11 +33,48 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
+    @Rule
+    public ReportHelper reportHelper = Factory.getReportHelper();
+
     @Test
     public void useAppContext() throws Exception {
+        reportHelper.label("myTestStepLabel");
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
-
         assertEquals("com.example.appcenter.sampleapp_android", appContext.getPackageName());
+
+        ViewInteraction textView = onView(
+                allOf(withText("Build"),
+                        childAtPosition(
+                                allOf(withId(R.id.pager_title_strip),
+                                        withParent(withId(R.id.container))),
+                                2),
+                        isDisplayed()));
+//        textView.perform(click());
+
+    }
+
+    @After
+    public void TearDown(){
+        reportHelper.label("Stopping App");
+    }
+
+    private static Matcher<View> childAtPosition(
+            final Matcher<View> parentMatcher, final int position) {
+
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Child at position " + position + " in parent ");
+                parentMatcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup) parent).getChildAt(position));
+            }
+        };
     }
 }
---
+++ appcenter/appcenter_test_run.bat
@@ -0,0 +1,2 @@
+copy ..\app\build\outputs\apk\androidTest\debug\app-debug-androidTest.apk C:\appcenter\appcenter-sampleapp-android\app-debug-androidTest.apk /y
+copy ..\app\build\outputs\apk\debug\app-debug.apk C:\appcenter\appcenter-sampleapp-android\app-debug.apk /y
